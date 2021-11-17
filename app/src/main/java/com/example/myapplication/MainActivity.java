62007af8d7af27bdcc62c31f63c4b8dcec8da302
ahmadnadeem123 <ahmadnadeempucit@gmail.com>
2021-11-17 21:10:27 +0500

    AlertDialog

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,68 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1=findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Alert Box");
+                builder.setTitle("Dialog");
+                builder.setCancelable(false);
+
+
+                builder.setPositiveButton(
+                        "Positive Button",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int asdf)
+                            {
+                                dialog.dismiss();
+                            }
+                        });
+                builder.setNegativeButton(
+                        "Negative Button",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+
+
+
+
+
+            }
+        });
+
+
 
 
        // Toast tost= Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG);
@@ -24,8 +72,8 @@ public class MainActivity extends AppCompatActivity {
         LayoutInflater layoutInflater = getLayoutInflater();
 
         View myLayout=layoutInflater.inflate(R.layout.layout2,null);
-
-
+        
+        
 
         Toast myToast=new Toast(getApplicationContext());
 
